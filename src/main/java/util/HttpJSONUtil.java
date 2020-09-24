30c507a5997e55bde945c27569883d938e72adb6
qizong007 <978451432@qq.com>
2020-09-24 11:46:01 +0800

    设置json格式

--- src/main/java/util/HttpJSONUtil.java
+++ src/main/java/util/HttpJSONUtil.java
@@ -29,45 +29,38 @@ public class HttpJSONUtil {
      * post请求
      *
      * */
-    public static String readContentFromPost(String url, String param) throws IOException{
-        String result = "";
-        BufferedReader in = null;
-        OutputStreamWriter out = null;
-        try {
-            //创建URL对象、
-            URL urlPost = new URL(url);
-            //打开URL连接
-            URLConnection connection = urlPost.openConnection();
-            HttpURLConnection conn = (HttpURLConnection) connection;
-            // 设置通用的请求属性
-            conn.setRequestProperty("accept", "*/*");
-            conn.setRequestProperty("connection", "Keep-Alive");
-            conn.setRequestProperty("user-agent","Mozilla/5.0 (Windows NT 6.1; WOW64; rv:47.0) Gecko/20100101 Firefox/47.0");
-            conn.setRequestMethod("POST");
-            // 发送POST请求必须设置如下两行
-            conn.setDoOutput(true);
-            conn.setDoInput(true);
-            // 获取URLConnection对象对应的输出流
-            out = new OutputStreamWriter(conn.getOutputStream(),"UTF-8");
-            // 发送请求参数
-            out.write(param);
-            // flush输出流的缓冲
-            out.flush();
-            // 定义BufferedReader输入流来读取URL的响应
-            in = new BufferedReader(
-                    new InputStreamReader(conn.getInputStream(),"UTF-8"));
-            String line;
-            while ((line = in.readLine()) != null) {
-                result += line;
-            }
-        } catch (MalformedURLException e) {
-            System.out.println("error");
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+    public static String readContentFromPost(String POST_URL, String params) throws IOException{
 
-        return result;
+        //URL postUrl = new URL(URLEncoder.encode(POST_URL,"utf-8"));
+        URL postUrl = new URL(POST_URL);
+        // 打开连接
+        HttpURLConnection connection = (HttpURLConnection) postUrl.openConnection();
+        connection.setDoOutput(true);
+        connection.setDoInput(true);
+        connection.setRequestMethod("POST");
+        // Post 请求不能使用缓存
+        connection.setUseCaches(false);
+        connection.setInstanceFollowRedirects(true);
+        connection.setRequestProperty("Content-Type","application/json");
+        // 连接，从postUrl.openConnection()至此的配置必须要在connect之前完成，
+        // 要注意的是connection.getOutputStream会隐含的进行connect。
+        connection.connect();
+        DataOutputStream out = new DataOutputStream(connection
+                .getOutputStream());
+        // DataOutputStream.writeBytes将字符串中的16位的unicode字符以8位的字符形式写道流里面
+        out.writeBytes(params);
+        out.flush();
+        out.close(); // flush and close
+        BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream(),"utf-8"));//设置编码,否则中文乱码
+        String line=null;
+        StringBuffer lines=new StringBuffer();
+        while ((line = reader.readLine()) != null){
+            lines.append(line);
+        }
+        reader.close();
+        // 断开连接
+        connection.disconnect();
+        return lines.toString();
     }
 
 }
